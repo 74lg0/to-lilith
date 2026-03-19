c01a2aad78f72b685365f1e7ba3b4576f695ac75
74lg0 <hack-z4lg0@proton.me>
2026-03-19 02:12:03 -0600

    Para mi mejor amiga :)

---
+++ static/script.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const boton = document.getElementById('button-principal');
+    const overlay = document.querySelector('.overlay');
+    
+    // Cancion
+    const audio = new Audio('/music/DarkBeach.mp3'); 
+
+    boton.addEventListener('click', () => {
+        audio.play().catch(error => {
+            console.log("Error de reproducción (revisa la ruta del archivo):", error);
+        });
+        if (overlay) {
+            overlay.style.opacity = '0';
+            overlay.style.visibility = 'hidden';
+            setTimeout(() => {
+                overlay.remove();
+            }, 1500);
+        }
+    });
+});
